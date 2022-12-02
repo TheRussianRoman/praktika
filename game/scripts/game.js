f3f5a3965a31c7dbc8f11a60deccc4261e6d612e
Roman <kosinovroman.sch@gmail.com>
2022-12-02 21:19:24 +0300

    added some images

--- game/scripts/game.js
+++ game/scripts/game.js
@@ -2,11 +2,19 @@ const cvs = document.getElementById("canvas")
 const ctx = cvs.getContext('2d');
 
 var bg = new Image();
+var monster = new Image();
+var knight = new Image();
 
-bg.src = "img/bg.png"; 
+//loading images
+monster.src = "img/monster.png";
+knight.src = "img/knight.png";
+bg.src = "img/bg2.png"; 
+
 
 function draw(){
     ctx.drawImage(bg,0,0);
+    ctx.drawImage(monster,(cvs.clientWidth-monster.width)/2, (cvs.clientHeight-monster.height)/2)
+    ctx.drawImage(knight,(cvs.clientWidth-knight.width)/2+100, (cvs.clientHeight-knight.height)/2);
 }
 
 bg.onload = draw;

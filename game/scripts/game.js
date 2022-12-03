bac9c8e294071965e1f5d69e06e01dcda61964fe
Roman <kosinovroman.sch@gmail.com>
2022-12-03 10:39:43 +0300

    Now you can enter first room!

--- game/scripts/game.js
+++ game/scripts/game.js
@@ -6,33 +6,106 @@ const ctx = cvs.getContext('2d');
 var bg = new Image();
 var battleRoom = new Image();
 var monster = new Image();
+var roomMonster = new Image();
 var knight = new Image();
 
 //loading images
 monster.src = "img/monster.png";
+roomMonster.src = "img/roomMonster.png";
 knight.src = "img/knight.png";
 battleRoom.src = "img/battleRoom.png"
 bg.src = "img/bg2.png"; 
 
 
 //canvas code
-function drawDefaultImage(){
-    ctx.clearRect(0,0,cvs.width,cvs.height);
+function drawDefaultRoom(){
+    clearCvs();
     ctx.drawImage(bg,0,0);
     ctx.drawImage(knight,(cvs.clientWidth-knight.width)/2, (cvs.clientHeight-knight.height)/2);
 }
 
-function drawBattleRoom(){
-    ctx
+function drawBattleRoom(roomID){
+    clearCvs();
+    ctx.drawImage(battleRoom, 0, 0)
+    if (roomObjects[roomID].objectID == "monster")
+     ctx.drawImage(roomMonster, (cvs.clientWidth-roomMonster.width)/2, (cvs.clientHeight-roomMonster.height)/2+50)
+}
+
+function clearCvs () {
+    ctx.clearRect(0,0,cvs.width,cvs.height);
 }
 
 //autoexec
-bg.onload = drawDefaultImage;
+bg.onload = drawDefaultRoom;
 
 //JS CODE
 //default JS settings
 let heroPower = 25;
 let roomObjects = new Array;
+let inBattle = false;
+let doorCoords = new Array;
+
+doorCoords.push({
+    xStart: 450,
+    yStart: 30,
+    xEnd: 560,
+    yEnd: 52
+})
+doorCoords.push({
+    xStart: 450,
+    yStart: 0,
+    xEnd: 0,
+    yEnd: 0
+})
+doorCoords.push({
+    xStart: 450,
+    yStart: 0,
+    xEnd: 0,
+    yEnd: 0
+})
+doorCoords.push({
+    xStart: 450,
+    yStart: 0,
+    xEnd: 0,
+    yEnd: 0
+})
+doorCoords.push({
+    xStart: 450,
+    yStart: 0,
+    xEnd: 0,
+    yEnd: 0
+})
+doorCoords.push({
+    xStart: 450,
+    yStart: 0,
+    xEnd: 0,
+    yEnd: 0
+})
+doorCoords.push({
+    xStart: 450,
+    yStart: 0,
+    xEnd: 0,
+    yEnd: 0
+})
+doorCoords.push({
+    xStart: 450,
+    yStart: 0,
+    xEnd: 0,
+    yEnd: 0
+})
+doorCoords.push({
+    xStart: 450,
+    yStart: 0,
+    xEnd: 0,
+    yEnd: 0
+})
+doorCoords.push({
+    xStart: 450,
+    yStart: 0,
+    xEnd: 0,
+    yEnd: 0
+})
+
 
 //generating rooms
 function generateRooms(){
@@ -40,11 +113,11 @@ function generateRooms(){
         let whatToSpawn = Math.floor(Math.random()*100)%2
         if (whatToSpawn == 0) {
             roomObjects.push(spawnRock(i));
-            console.log(roomObjects[i].objectID + " " + roomObjects[i].power + " in room number " + i)
+            console.log("Spawned " + roomObjects[i].objectID + " " + roomObjects[i].power + " in room number " + i)
         }
         if (whatToSpawn == 1) {
             roomObjects.push(spawnMonster(i));
-            console.log(roomObjects[i].objectID + " " + roomObjects[i].power + " in room number " + i)
+            console.log("Spawned " +roomObjects[i].objectID + " " + roomObjects[i].power + " in room number " + i)
         }
     }
 }
@@ -67,13 +140,40 @@ function spawnRock(){
     return monster;
 }
 
-function onDocumentClick(){
+function doorClick(e){
+    for (let i = 0; i<10; i++){
+        if (
+            (e.pageX >= doorCoords[i].xStart) &&
+            (e.pageY >= doorCoords[i].yStart) &&
+            (e.pageX <= doorCoords[i].xEnd) &&
+            (e.pageY <= doorCoords[i].yEnd)
+        ) return {
+            isDoor: true,
+            doorID: i
+        };
+        // else return {
+        //     isDoor: false
+        // }
+    }
 
+}
+
+function handleClick(e){
+    if (!inBattle) {
+        if(doorClick(e) !== undefined){
+            if (doorClick(e).isDoor == true){
+                drawBattleRoom(doorClick(e).doorID)
+            }
+            // console.log(doorClick(e))
+        }
+    }
 }
 
 //autoexec
 generateRooms();
-document.addEventListener("click", onDocumentClick)
+document.addEventListener("click", (e) => {
+    handleClick(e);
+})
 
 function sleep(ms){
     let start = Date.now();
---
+++ game/scripts/tmp-cursorPos.js
@@ -0,0 +1,8 @@
+let xPos = document.getElementById("xPos")
+let yPos = document.getElementById("yPos")
+
+document.addEventListener("click", (e) => {
+    // console.clear()
+    console.log(e.pageX);
+    console.log(e.pageY);
+})
